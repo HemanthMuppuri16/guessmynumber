5f5294eab69f6b3512593d1798165ba3a7cf9e85
HemanthMuppuri16 <muppurihemanth16@gmail.com>
2023-06-23 11:37:50 -0500

    new

---
+++ dist/index.672d4772.js
@@ -0,0 +1,44 @@
+"use strict";
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!!';
+// document.querySelector('.number').textContent = 3;
+// document.querySelector('.score').textContent = 40;
+// document.querySelector('.guess').value = 7;
+// console.log(document.querySelector('.guess').value);
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = function(message) {
+    document.querySelector(".message").textContent = message;
+};
+document.querySelector(".check").addEventListener("click", function() {
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess);
+    if (!guess && score > 1) displayMessage("Please enter a number!!");
+    else if (guess === secretNumber && score > 1) {
+        displayMessage("\uD83C\uDF89 Correct Number!!");
+        document.querySelector("body").style.backgroundColor = "#60b347";
+        document.querySelector(".number").style.width = "30rem";
+        document.querySelector(".number").textContent = secretNumber;
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector(".highscore").textContent = highscore;
+        }
+    } else if (guess !== secretNumber && score > 1) {
+        displayMessage(guess > secretNumber ? "\uD83D\uDCC8 Too High!!" : "\uD83D\uDCC9 Too Low!!");
+        score--;
+        document.querySelector(".score").textContent = score;
+    } else displayMessage("You lost the game \uD83D\uDE14");
+});
+document.querySelector(".again").addEventListener("click", function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector("body").style.backgroundColor = "#222";
+    displayMessage("Start guessing...");
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".guess").value = "";
+    document.querySelector(".number").style.width = "15rem";
+});
+
+//# sourceMappingURL=index.672d4772.js.map
